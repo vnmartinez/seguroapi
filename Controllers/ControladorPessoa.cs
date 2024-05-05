e23c65829792d3938a235d609b2065e36402abd9
vnmartinez <vn.martinez@aliare.co>
2024-05-05 16:55:05 -0300

    teste com trycatch

--- Controllers/ControladorPessoa.cs
+++ Controllers/ControladorPessoa.cs
@@ -9,23 +9,27 @@ namespace PROVA_N2.Controllers
         [HttpGet]
         public IActionResult Get([FromQuery] PessoaConsulta pessoaConsulta)
         {
-            if (pessoaConsulta == null) { 
-                return BadRequest("Dados inválidos");
+            try {
+                if (pessoaConsulta == null) { 
+                    return BadRequest("Dados inválidos");
+                }
+                var dados = new CarregaDadosCSV().Search();
+                
+                var registro = dados.FirstOrDefault(x => x.Age == pessoaConsulta.GetAgeRange() &&
+                                                        x.Gender == pessoaConsulta.Gender &&
+                                                        x.DrivingExperience == pessoaConsulta.GetDrivingExperienceRange() &&
+                                                        x.Education == pessoaConsulta.Education &&
+                                                        x.Income == pessoaConsulta.Income &&
+                                                        x.VehicleYear == pessoaConsulta.GetVehicleYear() &&
+                                                        x.VehicleType == pessoaConsulta.VehicleType &&
+                                                        x.AnnualMileage == pessoaConsulta.AnnualMileage);
+                if (registro == null) {
+                    return NotFound("Registro não encontrado");
+                }
+                return Ok(registro.CreditScore);   
+            } catch (System.Exception ex) {
+                return BadRequest("Entrou mas deu erro: " + ex.Message);
             }
-            var dados = new CarregaDadosCSV().Search();
-            
-            var registro = dados.FirstOrDefault(x => x.Age == pessoaConsulta.GetAgeRange() &&
-                                                     x.Gender == pessoaConsulta.Gender &&
-                                                     x.DrivingExperience == pessoaConsulta.GetDrivingExperienceRange() &&
-                                                     x.Education == pessoaConsulta.Education &&
-                                                     x.Income == pessoaConsulta.Income &&
-                                                     x.VehicleYear == pessoaConsulta.GetVehicleYear() &&
-                                                     x.VehicleType == pessoaConsulta.VehicleType &&
-                                                     x.AnnualMileage == pessoaConsulta.AnnualMileage);
-            if (registro == null) {
-                return NotFound("Registro não encontrado");
-            }
-            return Ok(registro.CreditScore);   
         }
     }
 }
